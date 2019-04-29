0ad01693f94f94c5e3f7298e96afb04c57f72c2f
zhuohai <1181316781@qq.com>
2019-04-29 18:16:34 +0800

    新增

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,14 @@
 import wxrequest from '@/utils/request';
 
 // const baseurl = 'https://www.sssyin.cn'
-const baseurl = 'https://uat.sssyin.cn';
+const baseurl = 'http://rest.apizza.net/mock/dc40447c3e43b777d38d9801fff7e670';
+
 //首页发现商品接口
-const hostGoodsList = (params) => wxrequest(params, baseurl + '/tour/oversea/getPlaceList');
+const hostGoodsList = (params) => wxrequest(params, baseurl + '/maiyou/api/indexlist');
+const getDetail = (params) => wxrequest(params, baseurl + '/maiyou/api/getdetail');
 const getimg = (params) => wxrequest(params, `https://openapi.mtlab.meitu.com/v3/makeup?api_key=CI6cZg01b3vVZ8oy1xpcPk3eLkU2LT_W&api_secret=qstwzvbfn9etMqtrzmnYrM0Pomjb8uqo`);
 export default {
 	hostGoodsList,
-	getimg
+	getimg,
+	getDetail
 }
